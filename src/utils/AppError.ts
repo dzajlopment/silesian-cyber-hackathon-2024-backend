72b190f50e2c8aa5dfd0254591598136c26d69f8
Paweł Bochyński <pawboc12@onet.pl>
2024-04-05 10:54:03 +0200

    Add AppError class util

---
+++ src/utils/AppError.ts
@@ -0,0 +1,15 @@
+class AppError extends Error {
+    statusCode: number
+    status: "fail" | "error";
+	isOperational: boolean;
+	constructor(message: string, statusCode: number) {
+		super(message);
+
+		this.statusCode = statusCode;
+		this.status = statusCode.toString().startsWith("4") ? "fail" : "error";
+		this.isOperational = true;
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+export default AppError
